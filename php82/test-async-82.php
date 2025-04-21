933751bf65cde246cd8da3e9f5f4bced77159517
Denis <denix4u@msn.com>
2025-04-21 15:47:43 +0100

    Initial commit

---
+++ php82/test-async-82.php
@@ -0,0 +1,36 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Promise\Utils;
+
+
+echo 'Running under PHP version: ' . phpversion() . PHP_EOL;
+
+$client = new Client();
+$start = microtime(true);
+
+// Async requests to httpbin.org with 5-second delay each
+$promises = [
+    $client->getAsync('https://httpbin.org/delay/5'),
+    $client->getAsync('https://httpbin.org/delay/5'),
+    $client->getAsync('https://httpbin.org/delay/5'),
+    $client->getAsync('https://httpbin.org/delay/5'),
+    $client->getAsync('https://httpbin.org/delay/5'),
+    $client->getAsync('https://httpbin.org/delay/5'),
+];
+
+// Wait for all promises to complete
+$responses = Utils::settle($promises)->wait();
+
+$end = microtime(true);
+$totalTime = round($end - $start, 2);
+
+echo "Total time: {$totalTime} seconds\n";
+
+// Optional: dump response status
+foreach ($responses as $index => $result) {
+    echo "Request #$index status: " . $result['value']->getStatusCode() . "\n";
+}
+
